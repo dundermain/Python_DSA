e31eee3b1ed139a19107d51ab69fbca108181add
SynapticLad <sachin68834@gmail.com>
2023-03-01 22:09:21 +0530

    added nested loops

--- Practice1.py
+++ Practice1.py
@@ -2,6 +2,7 @@
 This script is developed to practice some DSA questions
 Date: 01/03/2023
 '''
+
 #calculate the time complextity of the following function
 def foo(array):
     sum = 0
@@ -16,12 +17,14 @@ def foo(array):
     print("sum = "str(sum)+", product = "+str(product))
 #the answer is O(n)
 
+
 #calculate the time complexity of below code
 def printPairs(array):
     for i in array:
         for j in array:
             print(str(i)+","+str(j))
 #THe answer is o(n^2)
+
 
 #calculate the time complexity of the following code
 def printUnorderedPAirs(array):
@@ -30,3 +33,13 @@ def printUnorderedPAirs(array):
             print(array[i] + "," + array[j])
 #THe answer is O(n^2)
 
+
+#Find the time complexity of the following function
+def printUnorderedPairs(arrayA, arrayB):
+    for i in range(len(arrayA)):
+        for j in range(len(arrayB)):
+            if arrayA[i] < arrayB[j]:
+                print(str(arrayA[i]) + "," + arrayB[j])
+#The answer is O(ab) not O(n^2) because the loop will run for length of arrays 
+
+#Find the runtime or time complexity of the function
