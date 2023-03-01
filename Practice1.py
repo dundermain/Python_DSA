e32c6465a6744ce0b6976165ab50272cf1738829
SynapticLad <sachin68834@gmail.com>
2023-03-01 21:29:57 +0530

    added the answers

--- Practice1.py
+++ Practice1.py
@@ -3,7 +3,6 @@ This script is developed to practice some DSA questions
 Date: 01/03/2023
 '''
 #calculate the time complextity of the following function
-
 def foo(array):
     sum = 0
     product = 1
@@ -15,19 +14,19 @@ def foo(array):
         product *= 1
 
     print("sum = "str(sum)+", product = "+str(product))
-
+#the answer is O(n)
 
 #calculate the time complexity of below code
-
 def printPairs(array):
     for i in array:
         for j in array:
             print(str(i)+","+str(j))
-
+#THe answer is o(n^2)
 
 #calculate the time complexity of the following code
-
 def printUnorderedPAirs(array):
     for i in range(0, len(array)):
         for j in range(i+1, len(array)):
             print(array[i] + "," + array[j])
+#THe answer is O(n^2)
+
