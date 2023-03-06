4af0f11fb9cb88062223e94c7224574aa3a176b5
SynapticLad <sachin68834@gmail.com>
2023-03-06 21:24:48 +0530

    added new question

--- Practice2.py
+++ Practice2.py
@@ -19,5 +19,19 @@ def factorial(n):
     else:
         return n*factorial(n-1)
     
-#
+#FInd the runtime for the following algorithm
+def allFib(n):
+    for i in range(n):
+        print(str(i)+ ":,"+ str(fib(i)))
+
+def fib(n):
+    if n <=0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+    
+
+
 
