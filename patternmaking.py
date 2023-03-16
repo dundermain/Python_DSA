159b0a2550d165a123e24753219ac3dfd362c6f9
SynapticLad <sachin68834@gmail.com>
2023-03-16 20:17:42 +0530

    added the pattern function

---
+++ patternmaking.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def pattern(n,m):
+  assert m > 7, "Number of coloums should be greater than 7"
+  rows, cols = n ,m
+  pat = []
+
+  #printing middle welcome message
+  mid = ('-'*int((cols-7)/2)+'WELCOME'+'-'*int((cols-7)/2))
+  
+  #upper pattern
+  for i in range(int(n/2)):
+    barball = (".|."*(i*2+1))
+
+    underscore_no = int(int(m) - int(len(barball))/2)
+    up = ('-'*underscore_no+barball+'-'*underscore_no)
