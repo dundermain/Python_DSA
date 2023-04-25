3cd2c0a86bdb864b02d294289826a68ca352cdd1
SynapticLad <sachin68834@gmail.com>
2023-04-25 19:52:37 +0530

    Added OOPS concepts

--- OOPS.py
+++ OOPS.py
@@ -18,13 +18,25 @@ class SoftwareEngineer:
         print(f"{self.name} is writing a code in {langauge}")
 
 
-#some dunder methods available in Python
+#some dunder methods available in Python. dunder means double under methods
 
     def __str__(self):   #__str__ returns human readable string represenation of an object/instance 
         information = f"The name of the SE is {self.name}, age is {self.age}. {self.name} is working at level {self.level}"
         return information
     
-    def _eq__(self, other): #__eq__ returns
+    def _eq__(self, other): #__eq__ returns boolean as per our choice. UNlike ==, which compares object only, __eq__ can be used to compare attributes
+        return self.name == other.name and self.age == other.age
+    
+    #Now if we want a clas method that can work for instance/object as well
+    #we use static method decorator
+
+    @staticmethod  #static method can be used even without calling object. If we dont use static method, then the method can only be accessed by class and not by object/instance
+    def entry_salary(age):  #we cannot use self attribute here as static methods can be accesses without object/instance
+        if age<25:
+            return 5000
+        if age<30:
+            return 7000
+        return 9000
 
     
 
@@ -48,3 +60,5 @@ se2 = SoftwareEngineer("Max", 20, "Junior", 5000)
 print(se1 == se2) #This shpuld return True but it returns false because it compares the memory location. Since memory location will always be different 
 #hence it return false altough the object are identical to each other. Therefore we use __eq__ for comparison of objects attributes. Equality operator will
 #only check the object. If the object is same it will return true otherwise false. It doesnot care about whether the objects attributes are same or not
+
+print(SoftwareEngineer.entry_salary(27))
